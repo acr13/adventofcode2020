531930b5cda734325f26d979cee24eef2d55f108
Alex Richardson <alex@alexcr.com>
2020-12-03 10:06:42 -0500

    Day 3

---
+++ days/3.js
@@ -0,0 +1,34 @@
+const { readFileToGrid } = require('../helpers/read.js');
+
+const grid = readFileToGrid('3.txt');
+const position = [0,0];
+const LENGTH = grid[0].length;
+const TREE = '#';
+
+const traverse = (grid, moves) => {
+  let position = [0, 0];
+  let treeProduct = 1;
+
+  for (let i = 0; i < moves.length; i++) {
+    const deltaRow = moves[i][0];
+    const deltaCol = moves[i][1];
+    let trees = 0;
+
+    while (position[0] < grid.length) {
+      if (grid[position[0]][position[1]] === TREE) {
+        trees++;
+      }
+
+      position[0] = position[0] + deltaRow;
+      position[1] = (position[1] + deltaCol) % LENGTH;
+    }
+
+    treeProduct *= trees;
+    position = [0, 0];
+  }
+
+  return treeProduct;
+};
+
+console.log('Part one:', traverse(grid, [[1, 3]]));
+console.log('Part two:', traverse(grid, [[1, 1], [1, 3], [1, 5], [1, 7], [2, 1]]));
--- helpers/read.js
+++ helpers/read.js
@@ -4,8 +4,7 @@ const readFile = (fileName) => {
   const contents = [];
 
   try {
-    const data = fs.readFileSync(`./input/${fileName}`, 'UTF-8');
-    data
+    const data = fs.readFileSync(`./input/${fileName}`, 'UTF-8')
       .split(/\r?\n/)
       .map(line => contents.push(line));
   } catch (err) {
@@ -18,7 +17,22 @@ const readFile = (fileName) => {
 const readFileToIntegers = (fileName) =>
   readFile(fileName).map(s => parseInt(s));
 
+const readFileToGrid = (fileName) => {
+  const grid = [];
+
+  try {
+    const data = fs.readFileSync(`./input/${fileName}`, 'UTF-8')
+      .split(/\r?\n/)
+      .map(line => grid.push([...line.split('')]));
+  } catch (err) {
+    console.error(err);
+  }
+
+  return grid;
+}
+
 module.exports = {
   readFile,
   readFileToIntegers,
+  readFileToGrid,
 };
