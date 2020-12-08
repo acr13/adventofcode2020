a840798a0a2581b7a67483bcb13cd6bb1dd27540
Alex Richardson <alex@alexcr.com>
2020-12-08 08:33:08 -0500

    Day 8

---
+++ days/8.js
@@ -0,0 +1,45 @@
+const { readFile } = require('../helpers/read');
+
+const program = readFile('8.txt').map(line => line.split(' '));
+
+const run = (program) => {
+  const LENGTH = program.length;
+  const seen = {};
+
+  let i = 0;
+  let acc = 0;
+
+  while (i < LENGTH) {
+    if (seen[i] > 0) {
+      return { acc, isInfinite: true };
+    }
+
+    if (!seen[i]) { seen[i] = 0; }
+    seen[i]++;
+
+    const [inst, amt] = program[i];
+    if (inst === 'acc') {
+      acc += parseInt(amt);
+    }
+
+    inst === 'jmp' ? i += parseInt(amt) : i++;
+  }
+
+  return { acc, isInfinite: false };
+};
+
+const { acc, isInfinite } = run(program);
+console.log('Part one:', acc);
+
+for (let i = 0; i < program.length; i++) {
+  const [inst, amt] = program[i];
+  if (inst === 'jmp' || inst === 'nop') {
+    const copy = JSON.parse(JSON.stringify(program));
+    copy[i] = inst === 'jmp' ? 'nop' : 'jmp'
+    const { acc, isInfinite } = run(copy);
+    if (!isInfinite) {
+      console.log('Part two:', acc);
+      break;
+    }
+  }
+}
