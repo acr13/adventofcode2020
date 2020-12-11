2fb71bd41dd53aa45af751f684572ca0333df730
Alex Richardson <alex@alexcr.com>
2020-12-11 16:33:19 -0500

    refactoz

--- days/11.js
+++ days/11.js
@@ -1,6 +1,8 @@
 const { readFileToGrid } = require('../helpers/read');
 
 const GRID = readFileToGrid('11.txt');
+const R = GRID.length;
+const C = GRID[0].length;
 
 const numOccupied = (grid, r, c) =>
   [
@@ -12,94 +14,41 @@ const numOccupied = (grid, r, c) =>
     [r + 1, c - 1],
     [r + 1, c],
     [r + 1, c + 1],
-  ].filter(([r, c]) => r >= 0 && r < grid.length && c >= 0 && c < grid[0].length && grid[r][c] === '#').length;
+  ].filter(([r, c]) => r >= 0 && r < R && c >= 0 && c < C && grid[r][c] === '#').length;
+
+const findOccupiedSeatInThisDirection = (grid, r, c, diff) => {
+  let rr = r + diff[0];
+  let cc = c + diff[1];
+
+  while (true) {
+    if (rr < 0 || cc < 0 || rr >= R || cc >= C || grid[rr][cc] === 'L') {
+      return 0;
+    }
+    if (grid[rr][cc] === '#') {
+      return 1;
+    }
+
+    rr += diff[0];
+    cc += diff[1];
+  }
+};
 
 const numOccupied2 = (grid, r, c) => {
   let seats = 0;
   let cc = c;
   let rr = r;
   
-  // left
-  cc = c - 1;
-  while (true) {
-    if (cc < 0 || grid[r][cc] === 'L') break;
-    if (grid[r][cc] === '#') { seats++; break; }
-    cc--;
-  }
+  // left, right, up, down
+  seats += findOccupiedSeatInThisDirection(grid, r, c, [0, -1]);
+  seats += findOccupiedSeatInThisDirection(grid, r, c, [0, 1]);
+  seats += findOccupiedSeatInThisDirection(grid, r, c, [-1, 0]);
+  seats += findOccupiedSeatInThisDirection(grid, r, c, [1, 0]);
 
-  // right
-  cc = c;
-  cc = c + 1;
-  while (true) {
-    if (cc >= grid[0].length || grid[r][cc] === 'L') break;
-    if (grid[r][cc] === '#') { seats++; break; }
-    cc++;
-  }
-
-  // up
-  rr = r - 1;
-  while (true) {
-    if (rr < 0 || grid[rr][c] === 'L') break;
-    if (grid[rr][c] === '#') { seats++; break; }
-    rr--;
-  }
-
-  // down
-  rr = r;
-  rr = r + 1;
-  while (true) {
-    if (rr >= grid.length || grid[rr][c] === 'L') break;
-    if (grid[rr][c] === '#') { seats++; break; }
-    rr++;
-  }
-
-  // up and left
-  rr = r;
-  cc = c;
-  rr = r - 1;
-  cc = c - 1;
-  while (true) {
-    if (rr < 0 || cc < 0 || grid[rr][cc] === 'L') break;
-    if (grid[rr][cc] === '#') { seats++; break; }
-    rr--;
-    cc--;
-  }
-
-  // up and right
-  rr = r;
-  cc = c;
-  rr = r - 1;
-  cc = c + 1;
-  while (true) {
-    if (rr < 0 || cc >= grid[0].length || grid[rr][cc] === 'L') break;
-    if (grid[rr][cc] === '#') { seats++; break; }
-    rr--;
-    cc++;
-  }
-
-  // down and left
-  rr = r;
-  cc = c;
-  rr = r + 1;
-  cc = c - 1;
-  while (true) {
-    if (rr >= grid.length || cc < 0 || grid[rr][cc] === 'L') break;
-    if (grid[rr][cc] === '#') { seats++; break; }
-    rr++;
-    cc--;
-  }
-
-  // down and right
-  rr = r;
-  cc = c;
-  rr = r + 1;
-  cc = c + 1;
-  while (true) {
-    if (rr >= grid.length || cc >= grid[0].length || grid[rr][cc] === 'L') break;
-    if (grid[rr][cc] === '#') { seats++; break; }
-    rr++;
-    cc++;
-  }
+  // UL, UR, DL, DR
+  seats += findOccupiedSeatInThisDirection(grid, r, c, [-1, -1]);
+  seats += findOccupiedSeatInThisDirection(grid, r, c, [-1, 1]);
+  seats += findOccupiedSeatInThisDirection(grid, r, c, [1, -1]);
+  seats += findOccupiedSeatInThisDirection(grid, r, c, [1, 1]);
 
   return seats;
 }
@@ -129,7 +78,6 @@ const run = (grid) => {
 
 let last = 0;
 let lastGrid = GRID;
-
 
 while (true) {
   const { grid, seats } = run(lastGrid);
